d126313e74a6f9dc7de598e8e842f6f6581f026d
Boki <bogdanmitrovic17102001@gmail.com>
2019-06-16 22:29:49 +0200

    dodate klase

---
+++ Biblioteka/Knjiga.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteka
+{
+    class Knjiga
+    {
+        public int ID { get { return this.ID; } protected set { this.ID = value; } }
+        public string Naziv { get { return this.Naziv; } protected set { this.Naziv = value; } }
+        public string Pisac { get { return this.Pisac; } protected set { this.Pisac = value; } }
+        public string Izdavac { get { return this.Izdavac; } protected set { this.Izdavac = value; } }
+        public string Jezik { get { return this.Jezik; } protected set { this.Jezik = value; } }
+
+        public bool Dostupna { get { return this.Dostupna; } protected set { this.Dostupna = value; } }
+        public List<DateTime> Izdata { get { return this.Izdata; } protected set { this.Izdata = value; } }
+        public List<DateTime> Vracena { get { return this.Vracena; } protected set { this.Vracena = value; } }
+        public List<Ucenik> KodKogaJe { get { return this.KodKogaJe; } protected set { this.KodKogaJe = value; } }
+        /*diskutabilno za get set i liste*/
+        /*trenutno pravim tri liste, verovatno postoji lepsi nacin*/
+
+        /*mozda ako napravim podklasu za knjigu od istih izdavaca, naslova i pisaca, 
+         * da se ne pojavljuju vise puta u spisku ali da vrse normalno fje
+         videcemo*/
+        
+        public Knjiga(int ID)
+        {
+            this.ID = ID;
+        }
+        public Knjiga(int ID, string Naziv, string Pisac, string Izdavac, string Jezik)
+        {
+            this.ID = ID;
+            this.Naziv = Naziv;
+            this.Pisac = Pisac;
+            this.Izdavac = Izdavac;
+            this.Jezik = Jezik;
+            this.Dostupna = true;
+            this.Izdata = new List<DateTime>();
+            this.Vracena = new List<DateTime>();
+            this.KodKogaJe = new List<Ucenik>();
+        }
+
+        public void IzdajKnjigu(DateTime Datum, Ucenik Osoba)
+        {
+            if (this.Dostupna)
+            {
+                this.Dostupna = false;
+                this.KodKogaJe.Add(Osoba);
+                this.Izdata.Add(Datum);
+            }
+            /*
+             * else treba da se obavesti korisnik
+             */
+        }
+
+        public void VratiKnjigu(DateTime Datum)
+        {
+            if (this.Dostupna)
+            {
+                Dostupna = false;
+                this.Vracena.Add(Datum);
+            }
+            /*
+             * else treba da se obavesti korisnik
+             */
+        }
+    }
+}
---
+++ Biblioteka/Ucenik.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteka
+{
+    class Ucenik
+    {
+        public string Ime { get { return this.Ime; } protected set { this.Ime = value; } }
+        public char SrednjeSlovo { get { return this.SrednjeSlovo; } protected set { this.SrednjeSlovo = value; } }
+        public string Prezime { get { return this.Prezime; } protected set { this.Prezime = value; } }
+        public int Godina { get { return this.Godina; } protected set { this.Godina = value; } }
+        public int Odeljenje { get { return this.Odeljenje; } protected set { this.Odeljenje = value; } }
+        public int BrojUDnevniku { get { return this.BrojUDnevniku; } protected set { this.BrojUDnevniku = value; } }
+        public int BrojZaduzenihKnjiga { get { return this.BrojZaduzenihKnjiga; } protected set { this.BrojZaduzenihKnjiga = value; } }
+        public int Generacija { get { return this.Generacija; } protected set { this.Generacija = value; } }
+        public List<Knjiga> SpisakZaduzenihKnjiga { get { return this.SpisakZaduzenihKnjiga; } protected set { this.SpisakZaduzenihKnjiga = value; } }
+
+        public Ucenik(string Ime, char SrednjeSlovo, string Prezime, int Godina, int Odeljenje, int BrojUDnevniku, int BrojZaduzenihKnjiga, int Generacija)
+        {
+            this.Ime = Ime;
+            this.SrednjeSlovo = SrednjeSlovo;
+            this.Prezime = Prezime;
+            this.Godina = Godina;
+            this.Odeljenje = Odeljenje;
+            this.BrojUDnevniku = BrojUDnevniku;
+            this.BrojZaduzenihKnjiga = 0;
+            this.SpisakZaduzenihKnjiga = new List<Knjiga>();
+            this.Generacija = Generacija;
+        }
+        public string ReturnString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for(int i = 0; i < SpisakZaduzenihKnjiga.Count; i++)
+            {
+                if (stringBuilder.Length == 0) stringBuilder.Append(SpisakZaduzenihKnjiga[i].ID);
+                else stringBuilder.Append(SpisakZaduzenihKnjiga[i].ID + ",");
+            }
+            return stringBuilder.ToString();
+        }
+        //public Ucenik(string Ime, char SrednjeSlovo, string Prezime, int Godina, int Odeljenje, int BrojUDnevniku, int BrojZaduzenihKnjiga, string IdKnjiga)
+        //    : this(Ime, SrednjeSlovo, Prezime, Godina, Odeljenje, BrojUDnevniku, BrojZaduzenihKnjiga)
+        //{
+        //    string[] ZaduzeneKnjige = IdKnjiga.Split(',');
+        //    this.BrojZaduzenihKnjiga = ZaduzeneKnjige.Length;
+
+        //    for(int i=0;i<this.BrojZaduzenihKnjiga; i++)
+        //    {
+        //        this.SpisakZaduzenihKnjiga.Add(new Knjiga(int.Parse(ZaduzeneKnjige[i])));
+        //    }
+        //}
+
+    }
+}
